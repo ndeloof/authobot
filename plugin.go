4ce20e90b8ccb2fa5ed7784a1ff7962cd8c45e21
Nicolas De Loof <nicolas.deloof@gmail.com>
2017-07-05 14:38:23 +0200

    use a whitelist to filter authorized commands

--- plugin.go
+++ plugin.go
@@ -12,6 +12,8 @@ import (
 	"github.com/docker/go-plugins-helpers/authorization"
 	"github.com/docker/engine-api/types/container"
 	"github.com/docker/engine-api/types/mount"
+	"github.com/pkg/errors"
+	"fmt"
 )
 
 func newPlugin(dockerHost string) (*authobot, error) {
@@ -25,6 +27,27 @@ func newPlugin(dockerHost string) (*authobot, error) {
 
 var (
 	create = regexp.MustCompile(`/containers/create`)
+
+	whitelist = []regexp.Regexp{
+		*regexp.MustCompile(`/_ping`),
+		*regexp.MustCompile(`/version`),
+
+		*regexp.MustCompile(`/containers/create`),
+		*regexp.MustCompile(`/containers/*/start`),
+		*regexp.MustCompile(`/containers/*/stop`),
+		*regexp.MustCompile(`/containers/*/kill`),
+		*regexp.MustCompile(`/containers/*/json`), // inspect
+		*regexp.MustCompile(`/containers/*/exec`),
+		*regexp.MustCompile(`/exec/*/start`),
+		*regexp.MustCompile(`/exec/*/json`),
+
+		*regexp.MustCompile(`/build`),
+		*regexp.MustCompile(`/images/create`), // pull
+		*regexp.MustCompile(`/images/*/json`), // inspect
+		*regexp.MustCompile(`/images/*/push`),
+		*regexp.MustCompile(`/images/*/tag`),
+		*regexp.MustCompile(`/images/*`), // remove
+	}
 )
 
 type authobot struct {
@@ -37,18 +60,27 @@ type configWrapper struct {
 }
 
 func (p *authobot) AuthZReq(req authorization.Request) authorization.Response {
-	ruri, err := url.QueryUnescape(req.RequestURI)
+	uri, err := url.QueryUnescape(req.RequestURI)
 	if err != nil {
 		return authorization.Response{Err: err.Error()}
 	}
-	if req.RequestMethod == "POST" && create.MatchString(ruri) {
+
+	fmt.Println("checking request to "+uri+" from user : "+req.User);
+
+	err = p.Authorized(uri);
+	if err != nil {
+		return authorization.Response{Err: err.Error()}
+	}
+
+
+	if req.RequestMethod == "POST" && create.MatchString(uri) {
 		if req.RequestBody != nil {
 			body := &configWrapper{}
 			if err := json.NewDecoder(bytes.NewReader(req.RequestBody)).Decode(body); err != nil {
 				return authorization.Response{Err: err.Error()}
 			}
 			if len(body.Volumes) > 0 {
-				return authorization.Response{Msg: "use of bind mounts is not allowed"}
+				return authorization.Response{Msg: "use of volumes is not allowed"}
 			}
 			if body.HostConfig.Privileged {
 				return authorization.Response{Msg: "use of Privileged contianers is not allowed"}
@@ -59,9 +91,20 @@ func (p *authobot) AuthZReq(req authorization.Request) authorization.Response {
 				}
 
 			}
+			return authorization.Response{Allow: true}
 		}
 	}
-	return authorization.Response{Allow: true}
+
+	return authorization.Response{Msg: uri + " API is not allowed"}
+}
+
+func (p *authobot) Authorized(uri string) error {
+	for _, m := range whitelist {
+		if m.MatchString(uri) {
+			return nil;
+		}
+	}
+	return errors.New(uri + " is not authorized")
 }
 
 func (p *authobot) AuthZRes(req authorization.Request) authorization.Response {
